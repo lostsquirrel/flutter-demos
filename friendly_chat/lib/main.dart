09c975a21703693353b1c59fab974809eb260b48
lostsquirrel <lostsquirreli@hotmail.com>
2022-04-14 22:56:36 +0800

    add composer message

--- friendly_chat/lib/main.dart
+++ friendly_chat/lib/main.dart
@@ -4,10 +4,40 @@ void main() {
   runApp(const ChatScreen());
 }
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final _textController = TextEditingController();
+
+  Widget _buildTextComposer() {
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).colorScheme.secondary),
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(children: [
+          Flexible(
+            child: TextField(
+              controller: _textController,
+              onSubmitted: _handleSubmitted,
+              decoration:
+                  const InputDecoration.collapsed(hintText: "Send a message"),
+            ),
+          ),
+          Container(
+              margin: const EdgeInsets.symmetric(horizontal: 4.0),
+              child: IconButton(
+                  icon: const Icon(Icons.send),
+                  onPressed: () => _handleSubmitted(_textController.text)))
+        ]),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,7 +46,10 @@ class ChatScreen extends StatelessWidget {
         appBar: AppBar(
           title: const Text("FriendlyChat"),
         ),
+        body: _buildTextComposer(),
       ),
     );
   }
+
+  void _handleSubmitted(String value) {}
 }
