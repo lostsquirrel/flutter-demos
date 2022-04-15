8c666811225f353bc51231424dfc836e8d2edc50
lostsquirrel <lostsquirreli@hotmail.com>
2022-04-15 11:28:55 +0800

    add message box

--- friendly_chat/lib/main.dart
+++ friendly_chat/lib/main.dart
@@ -1,11 +1,62 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const ChatScreen());
+  runApp(const FriendlyChatApp());
+}
+
+class FriendlyChatApp extends StatelessWidget {
+  const FriendlyChatApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'FriendlyChat',
+      home: ChatScreen(),
+    );
+  }
+}
+
+class ChatMessage extends StatelessWidget {
+  ChatMessage({
+    required this.text,
+    Key? key,
+  }) : super(key: key);
+  final String text;
+  String _name = 'Flutter';
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: CircleAvatar(child: Text(_name[0])),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                _name,
+                style: Theme.of(context).textTheme.headline4,
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 5.0),
+                child: Text(text),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
 }
 
 class ChatScreen extends StatefulWidget {
-  const ChatScreen({Key? key}) : super(key: key);
+  const ChatScreen({
+    Key? key,
+  }) : super(key: key);
 
   @override
   State<ChatScreen> createState() => _ChatScreenState();
@@ -13,6 +64,37 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   final _textController = TextEditingController();
+  final List<ChatMessage> _messages = [];
+  final FocusNode _focusNode = FocusNode();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("FriendlyChat"),
+      ),
+      body: Column(
+        children: [
+          Flexible(
+            child: ListView.builder(
+              padding: const EdgeInsets.all(8.0),
+              reverse: true,
+              itemBuilder: (_, index) => _messages[index],
+              itemCount: _messages.length,
+            ),
+          ),
+          const Divider(
+            height: 1.0,
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).cardColor,
+            ),
+            child: _buildTextComposer(),
+          ),
+        ],
+      ),
+    );
+  }
 
   Widget _buildTextComposer() {
     return IconTheme(
@@ -26,30 +108,29 @@ class _ChatScreenState extends State<ChatScreen> {
               onSubmitted: _handleSubmitted,
               decoration:
                   const InputDecoration.collapsed(hintText: "Send a message"),
+              focusNode: _focusNode,
             ),
           ),
           Container(
-              margin: const EdgeInsets.symmetric(horizontal: 4.0),
-              child: IconButton(
-                  icon: const Icon(Icons.send),
-                  onPressed: () => _handleSubmitted(_textController.text)))
+            margin: const EdgeInsets.symmetric(horizontal: 4.0),
+            child: IconButton(
+              icon: const Icon(Icons.send),
+              onPressed: () => _handleSubmitted(_textController.text),
+            ),
+          )
         ]),
       ),
     );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'FriendlyChat',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("FriendlyChat"),
-        ),
-        body: _buildTextComposer(),
-      ),
+  void _handleSubmitted(String text) {
+    _textController.clear();
+    var message = ChatMessage(
+      text: text,
     );
+    setState(() {
+      _messages.insert(0, message);
+    });
+    _focusNode.requestFocus();
   }
-
-  void _handleSubmitted(String value) {}
 }
