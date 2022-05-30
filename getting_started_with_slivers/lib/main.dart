8c94345c72cb828f65aa71593ee476f683fe73dc
lisong <lisong@cdsunrise.net>
2022-05-30 18:17:09 +0800

    init project

---
+++ getting_started_with_slivers/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import 'scroll_behavior.dart';
+
+void main() {
+  runApp(const HorizonsApp());
+}
+
+class HorizonsApp extends StatelessWidget {
+  const HorizonsApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Horizons Weather',
+      theme: ThemeData.dark(),
+      scrollBehavior: const ConstantScrollBehavior(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Horizons"),
+          backgroundColor: Colors.teal,
+        ),
+        body: const WeeklyForecastList(),
+      ),
+    );
+  }
+}
---
+++ getting_started_with_slivers/lib/scroll_behavior.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ConstantScrollBehavior extends ScrollBehavior {
+  const ConstantScrollBehavior();
+
+  @override
+  Widget buildScrollbar(
+          BuildContext context, Widget child, ScrollableDetails details) =>
+      child;
+
+  @override
+  Widget buildOverscrollIndicator(
+      BuildContext context, Widget child, ScrollableDetails details) {
+    return child;
+  }
+
+  @override
+  ScrollPhysics getScrollPhysics(BuildContext context) {
+    return const BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics());
+  }
+}
